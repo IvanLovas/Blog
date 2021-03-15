f214b7dc6b3c2ccd9650b43788cc7766dbb78eee
IvanLovas <lovass19@gmail.com>
2021-03-15 21:36:11 +0100

    peti zadatak

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php include('db.php')?>
-<?php
-    if (isset($_GET['post_id'])) {
-    $sql = "SELECT * FROM posts WHERE posts.id = {$_GET['post_id']}";
-    $singlePost = getDataFromSinglePost($connection, $sql);
-    }
-?>
 
+<?php
+    $sql = "SELECT * FROM posts ORDER BY posts.created_at DESC";
+    $posts = getDataFromDatabase($connection, $sql);
+?>
 
 <!doctype html>
 <html lang="en">
@@ -21,24 +19,36 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
     <!-- Custom styles for this template -->
     <link href="styles/blog.css" rel="stylesheet">
     <link rel="stylesheet" href="./styles/styles.css">
 </head>
 
 <body>
+
 <?php include('header.php') ?>
 
 <main role="main" class="container">
     <div class="row">
         <div class="col-sm-8 blog-main">
+        <?php
+            foreach ($posts as $post) {
+        ?>
             <div class="blog-post">
-                <h2 class="blog-post-title"><?php echo($singlePost['title']) ?></h2>
-                <p class="blog-post-meta"><?php echo($singlePost['created_at']) ?> <a href="#"> <?php echo($singlePost['author']) ?></a></p>
+                <a href="single-post.php?post_id=<?php echo ($post['id']) ?>" class="blog-post-title"><?php echo($post['title']) ?></a>
+                <p class="blog-post-meta"><?php echo($post['created_at']) ?><a href="#"> <?php echo($post['author']) ?></a></p>
+                <p> <?php echo($post['body']) ?></p>
+            </div><!-- /.blog-post -->
+        <?php
+            }    
+        ?>   
 
-                <p> <?php echo($singlePost['body']) ?></p>
-            </div>
+
+            <nav class="blog-pagination">
+                <a class="btn btn-outline-primary" href="#">Older</a>
+                <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+            </nav>
+
         </div><!-- /.blog-main -->
         <?php include('sidebar.php') ?>
         <!-- /.blog-sidebar -->
